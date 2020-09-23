72f18664077e2e92ad226cfd4e72c13b22d31e1d
bajaco <brett@agyx.org>
2020-09-23 18:45:01 +0000

    Added comments and ability to specify separate directory for the keyfile.

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Quick and dirty password manager.
 
 ### Usage
 
-Set the FILES_DIR in pass.py.
-Upon running the program you are presented with a blank input screen. 
-Enter the name of the site you're logging into. If a password exists it will retrieve it, otherwise it will generate one at random.
-Either way it will copy it to the clipboard.
+1. Set the FILES_DIR in pass.py.
+2. Set the KEY_DIR in pass.py.
+3. Run the python script with the site name as an argument or without Example: `python pass.py github` or `python pass.py`
+4. If run without command line argument enter the site name now.
+5. The password will be copied to the system clipboard. If it does not exist it will be generated first.
 
-Optionally set the site by command line argument
 
-The strings are encrypted, which serves no purpose as the key is stored in the same directory. However it would be trivial in the future to add a key location and more options. I made this simply because I have a bad habit of reusing passwords which I am trying to break, and this is marginally better than writing them down.
+The strings are encrypted, which serves no purpose as the key is in the same directory, but may be useful if the KEY_DIR is set to an external drive such as `/mnt/usb`.
+I made this simply because I have a bad habit of reusing passwords which I am trying to break, and this is marginally better than writing them down.
 
+Use at your own risk!
--- pass.py
+++ pass.py
@@ -5,45 +5,70 @@ import random
 import string
 import sys
 
+#Set to permanent location of database 
 FILES_DIR=''
 
+#Set to location of keydir. eg /mnt/usb to store key on external drive
+KEY_DIR=''
+
+#Create the key
 def write_key():
     key = Fernet.generate_key()
-    with open(FILES_DIR + '/key.key', 'wb') as key_file:
+    with open(KEY_DIR + '/key.key', 'wb') as key_file:
         key_file.write(key)
         #print('key generated')
 
+#Load the key
 def load_key():
     try:
-        return open(FILES_DIR + '/key.key', 'rb').read()
+        return open(KEY_DIR + '/key.key', 'rb').read()
         #print('key loaded')
+    
+    #If keyloading fails create key and load it
     except OSError:
         #print('could not open key')
         #print('generating new key')
         write_key()
         load_key()
 
+#Create table
 def create_table(conn):
     c = conn.cursor()
     try:
         c.execute('''CREATE TABLE login
                 (site text, pass text)''')
         conn.commit()
+    
+    #if table exists do nothing
     except sqlite3.Error:
         pass
 
+#load key
 key = load_key()
 f = Fernet(key)
+
+#connect to database
 conn = sqlite3.connect(FILES_DIR + '/pass.db')
+
+#create table if required
 create_table(conn)
+
 c = conn.cursor()
+
 site = None
+
+#if an argument is supplied use it as site
 if len(sys.argv) == 2:
     site = sys.argv[1]
+
+#use input as site
 else:
     site = input()
+
 c.execute('SELECT * FROM login WHERE site=?', (site,))
 res = c.fetchall()
+
+#if password does not exist create it
 if len(res) == 0:
     characters = string.ascii_letters + string.digits + string.punctuation
     password = []
@@ -56,6 +81,8 @@ if len(res) == 0:
     insert = (site, password,)
     c.execute('INSERT INTO login VALUES (?,?)', insert)
     conn.commit()
+
+#if password exists use it
 else:
     res = res[0]
     password = res[1]
